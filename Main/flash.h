7357da9114e8284e1c800220c4c29ac257f5a723
Cher Liang <liang14032@gmail.com>
2023-03-16 21:43:26 +0000

    fix default constructor mistake

--- Main/flash.h
+++ Main/flash.h
@@ -3,29 +3,30 @@
 #include "Arduino.h"
 
 #include "FlashStorage_STM32.hpp"
-struct PeatData{
+struct PeatData
+{
   int16_t peat_height;
   int16_t temperature;
-  uint16_t pressure; 
+  uint16_t pressure;
 };
 
 class FlashStorage
 {
-private:    
-  uint16_t eeAddress=0;
+private:
+  uint16_t eeAddress = 0;
   uint16_t eeReadAddress = 0;
-  
+
   uint16_t eeAddressMax;
 
 public:
-  FlashStorage(int);
+  FlashStorage();
+  void getFlashStorageInfo();
   // void writeFlashTimestamp(uint32_t);
   void writeFlashData(PeatData);
   void clearFlash();
   // uint32_t getFlashInit();
   PeatData getFlashData();
   // uint32_t getFlashEnd();
-
 };
 
 #endif
